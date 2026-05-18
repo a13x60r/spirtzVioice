85bcd5bc8b6690ba6a6a88ce00fb839a58fcae2f
a13x60r <20254616+a1bo@users.noreply.github.com>
2026-05-18 16:52:20 +0200

    fix: load onnx runtime from piper dist

--- src/workers/tts/OfflineVoice.ts
+++ src/workers/tts/OfflineVoice.ts
@@ -117,7 +117,7 @@ export class OfflineVoice {
                 (_p) => { /* Silenced internal logs */ },
                 null, // phonemeIds
                 false, // inferEmotion
-                this.toAbsoluteUrl(BASE_URL), // onnxruntimeUrl (folder)
+                this.toAbsoluteUrl(BASE_URL + 'dist/'), // onnxruntimeUrl (folder containing ort.min.js)
                 lengthScale,
                 useWebGPU,
                 gpuPreference
